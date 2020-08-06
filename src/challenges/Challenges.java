835dfdb100f5f9c2c8281cb3088b9289bd7ff25f
Konrad <zagozdakonrad@gmail.com>
2020-08-06 18:29:37 +0200

    add "calculate mean of digits" solution

--- src/challenges/Challenges.java
+++ src/challenges/Challenges.java
@@ -3,14 +3,31 @@ package challenges;
 public class Challenges {
     // https://edabit.com/challenge/ocqRdAXXniLS4ea3h
     public static boolean checkEquality(Object a, Object b) {
-        if (a == null && b == null) { return true;}
-        if (a == null || b == null) { return false;}
+        if (a == null && b == null) {
+            return true;
+        }
+        if (a == null || b == null) {
+            return false;
+        }
         return a.equals(b);
     }
 
     // https://edabit.com/challenge/LJh54oryEc3tkagzD
     public static String correctSpacing(String sentence) {
         return sentence.trim().replaceAll("\\s+", " ");
+    }
+
+    // https://edabit.com/challenge/mvHZK6Dy3ZF8EFoG3
+    public static int mean(int a) {
+        int digits = 0;
+        int sum = 0;
+        while (a > 0) {
+            sum += a % 10;
+            digits++;
+            a /= 10;
+        }
+        if (digits == 0) return 0;
+        return sum / digits;
     }
 
 }
--- src/challenges/Main.java
+++ src/challenges/Main.java
@@ -1,8 +1,11 @@
 package challenges;
 
+import static challenges.Challenges.mean;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("halo");
+        System.out.println(mean(55));
     }
 }
